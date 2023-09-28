59ada10e1a9403197fbdd33189c27acc4561da96
Ismael-Njihia <engishmaelnjihia@gmail.com>
2023-09-28 19:19:29 +0300

    rid of the container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,10 @@ const App = () => {
     <>
     <Header />
     <main>
-      <Container>
+      
         <ToastContainer />
         <Outlet />
-      </Container>
+      
     </main>
     </>
   )
