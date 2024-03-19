8471d7ad85925a3d809794b3a76e2feb06ef272d
Ismael njihia <engishmaelnjihia@gmail.com>
2024-03-19 21:46:48 +0300

    added the script code on setupTests.js

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,5 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9904561648370283"
+     crossorigin="anonymous"></script>
